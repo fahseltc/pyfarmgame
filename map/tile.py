87f4fb4bc64dfcc450d61da801c5d7f0962b2012
Charles Fahselt <charles.fahselt@hp.com>
2016-08-15 13:23:43 -0500

    adding attributes list to tiles. image is now an attribute of a tile, and tiles may or may not support a given attribute

--- map/tile.py
+++ map/tile.py
@@ -9,11 +9,19 @@ class Tile:
     def __init__(self, coords, position, image, type_in):
         self.coords = coords
         self.position = position
+        self.attributes = {}
         if image is not None:
             self.image = self.scale_image(image)
+            self.add_attribute('image', self.scale_image(image))
         else:
             self.image = None;
         self.type = type_in
 
     def scale_image(self, image):
         return pygame.transform.scale(image, (image.get_width() * CONSTANTS.TILE_SCALING, image.get_height() * CONSTANTS.TILE_SCALING))
+
+    def get_attribute(self, key):
+        return self.attributes.get(key, False)
+
+    def add_attribute(self, key, attribute):
+        self.attributes[key] = attribute
